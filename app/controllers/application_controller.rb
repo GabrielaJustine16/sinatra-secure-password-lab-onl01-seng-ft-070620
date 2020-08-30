4827534e583f65f75daf92bb2bd7d6381bc094f9
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-30 21:21:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(username: params[:username], password: params[:password])
-    if user.save
-    # if user.save && params[:username] != ""       #=> use if User class doesn't have line 4
-      redirect "/login"
-    else
-      redirect "/failure"
-    end
+    user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
   end
 
   get '/account' do
@@ -31,18 +30,19 @@ class ApplicationController < Sinatra::Base
     erb :account
   end
 
+
   get "/login" do
     erb :login
   end
 
   post "/login" do
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/account"
-    else
-      redirect "/failure"
-    end
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/account"
+		else
+			redirect "/failure"
+		end
   end
 
   get "/failure" do
